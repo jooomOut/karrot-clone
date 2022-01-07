fc9a53fd1e712d94d2a628f7a9573e7a287cb404
jooomOut <rimeilo324@naver.com>
2022-01-07 11:39:43 +0900

    Initial commit

---
+++ src/main/java/com/karrot/demo/NumbleKarrotApplication.java
@@ -0,0 +1,13 @@
+package com.karrot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbleKarrotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumbleKarrotApplication.class, args);
+    }
+
+}
