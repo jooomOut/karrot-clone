e2b70b2b7ab09e9393bef35958c4e41ad62e5d87
jooomOut <rimeilo324@naver.com>
2022-01-08 13:21:29 +0900

    Feat: 사용자 유니크 키 설정
    이메일, 전화번호는 고유해야 함

--- src/main/java/com/karrot/demo/domain/user/User.java
+++ src/main/java/com/karrot/demo/domain/user/User.java
@@ -2,22 +2,30 @@ package com.karrot.demo.domain.user;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Builder
 @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter
 @Entity
+@Table(
+        name="user",
+        uniqueConstraints={
+                @UniqueConstraint(
+                        columnNames={"email","phone"}
+                )
+        }
+)
 public class User {
     @Id
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name="email")
     private String email;
     private String password;
     private String username;
+    @Column(name="phone")
     private String phone;
     private String nickname;
 
