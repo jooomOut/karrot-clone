306c8e628b08d07258bc9e0df64e944587c8bb2d
jooomOut <rimeilo324@naver.com>
2022-01-13 19:30:50 +0900

    Test: 이미지 업로드 기능 구현

--- src/main/java/com/karrot/demo/service/ImageService.java
+++ src/main/java/com/karrot/demo/service/ImageService.java
@@ -5,18 +5,20 @@ import com.karrot.demo.domain.image.ImageRepository;
 import com.karrot.demo.domain.item.Item;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Slf4j
 @Service
 public class ImageService {
 
-    private final String UPLOAD_PATH = "/img/test";
+    private final String UPLOAD_PATH = "/items";
 
     private ImageRepository imageRepository;
 
@@ -25,21 +27,27 @@ public class ImageService {
         this.imageRepository = imageRepository;
     }
 
-    public void upload(Item item, List<MultipartFile> files){
+    public void upload(Item item, List<MultipartFile> files) {
         if (files != null){
             for (MultipartFile file : files){
-                upload(item, file);
+                try {
+                    upload(item, file);
+                } catch (IOException e){
+                    log.info("Can not upload file");
+                }
+
             }
         }
     }
 
-    private void upload(Item item, MultipartFile imageFile) {
-        String fileName = imageFile.getOriginalFilename();
-        File targetFile = new File(UPLOAD_PATH, fileName);
-        if (!new File(UPLOAD_PATH).exists()) {
-            new File(UPLOAD_PATH).mkdirs();
-        }
+    public void upload(Item item, MultipartFile imageFile) throws IOException {
+        String fileName = imageFile.getName();
+        String path = new ClassPathResource("/static/img").getFile().getAbsolutePath() + UPLOAD_PATH;
 
+        File targetFile = new File(path, fileName);
+        if (!new File(path).exists()) {
+            new File(path).mkdirs();
+        }
         //파일 복사
         try {
             FileCopyUtils.copy(imageFile.getBytes(), targetFile);
