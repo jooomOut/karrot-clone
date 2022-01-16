19053b5f93550fdc960bdac20ab954751b5ae3b7
jooomOut <rimeilo324@naver.com>
2022-01-16 17:29:35 +0900

    Feat: 프로필 변경 API 추가

--- src/main/java/com/karrot/demo/service/UserService.java
+++ src/main/java/com/karrot/demo/service/UserService.java
@@ -10,17 +10,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.EntityNotFoundException;
 
 @Slf4j
 @Service
 public class UserService {
 
+    private ImageService imageService;
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder,
+                       ImageService imageService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.imageService = imageService;
     }
 
     public void registerUser(RegisterUserDto userDto){
@@ -34,6 +40,18 @@ public class UserService {
         } catch(DataIntegrityViolationException e){ // 무결성 조건 (중복) - 이메일
             throw new DuplicateUserException();
         }
+    }
+
+    public void updateProfile(Long userId, MultipartFile image, String nickname){
+        if (nickname == null || nickname.equals("")){
+            throw new IllegalArgumentException("닉네임은 빈 값일 수 없습니다.");
+        }
+        Account user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("user not found with id" + userId));
+        user.setNickname(nickname);
+        userRepository.save(user);
+
+        //imageService.
     }
 
     public Account toEntity(RegisterUserDto userDto){
