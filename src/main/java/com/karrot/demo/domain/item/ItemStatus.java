e907cd4036eafc909d03780d29d6c39c8d90e6cd
jooomOut <rimeilo324@naver.com>
2022-01-21 12:57:53 +0900

    Feat: 게시물 상태 enum 한글화

--- src/main/java/com/karrot/demo/domain/item/ItemStatus.java
+++ src/main/java/com/karrot/demo/domain/item/ItemStatus.java
@@ -1,5 +1,16 @@
 package com.karrot.demo.domain.item;
 
 public enum ItemStatus {
-    SALE, RESERVED, SOLD
+    SALE("판매중"),
+    RESERVED("예약중"),
+    SOLD("거래완료");
+
+    private String krName;
+    ItemStatus(String krName) {
+        this.krName = krName;
+    }
+    public String getKrName() {
+        return krName;
+    }
+
 }
