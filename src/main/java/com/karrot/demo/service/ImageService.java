7fa26ca809802254415ec6b0455b2f4f48c715b8
jooomOut <rimeilo324@naver.com>
2022-01-22 12:02:35 +0900

    Feat: 프로필 사진 삭제 기능 구현

--- src/main/java/com/karrot/demo/service/ImageService.java
+++ src/main/java/com/karrot/demo/service/ImageService.java
@@ -1,7 +1,7 @@
 package com.karrot.demo.service;
 
-import com.karrot.demo.domain.image.ItemImageRepository;
 import com.karrot.demo.domain.image.ItemImage;
+import com.karrot.demo.domain.image.ItemImageRepository;
 import com.karrot.demo.domain.image.UserProfileImage;
 import com.karrot.demo.domain.image.UserProfileImageRepository;
 import com.karrot.demo.domain.item.Item;
@@ -16,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
@@ -25,12 +26,12 @@ public class ImageService {
     private final String ITEM_PATH = "/items";
     private final String USER_PROFILE_PATH = "/user_profile";
 
-    private ItemImageRepository imageImageRepository;
+    private ItemImageRepository itemImageRepository;
     private UserProfileImageRepository userProfileImageRepository;
 
     @Autowired
-    public ImageService(ItemImageRepository imageImageRepository, UserProfileImageRepository userProfileImageRepository) {
-        this.imageImageRepository = imageImageRepository;
+    public ImageService(ItemImageRepository itemImageRepository, UserProfileImageRepository userProfileImageRepository) {
+        this.itemImageRepository = itemImageRepository;
         this.userProfileImageRepository = userProfileImageRepository;
     }
 
@@ -52,12 +53,14 @@ public class ImageService {
         File targetFile = storeImageFile(image, ITEM_PATH);
 
         ItemImage itemImage = toEntity(targetFile, item);
-        imageImageRepository.save(itemImage);
+        itemImageRepository.save(itemImage);
     }
 
     public void upload(Account account, MultipartFile image) throws IOException {
         if (image == null) return;
-
+        if (account.getImage() != null) {
+            userProfileImageRepository.delete(account.getImage());
+        }
         File targetFile = storeImageFile(image, USER_PROFILE_PATH);
 
         UserProfileImage profileImage = toEntity(targetFile, account);
@@ -80,7 +83,39 @@ public class ImageService {
         }
         return targetFile;
     }
+    public void deleteImage(Long imageId, String type){
+        switch (type) {
+            case "user_profile" : deleteUserProfileImage(imageId); break;
+            case "item" : deleteItemImage(imageId); break;
+        }
+    }
 
+    private void deleteItemImage(Long imageId) {
+        Optional<ItemImage> optionalItemImage = itemImageRepository.findById(imageId);
+        if (optionalItemImage.isPresent()){
+            ItemImage itemImage = optionalItemImage.get();
+            deleteFile("/static" + itemImage.getPath());
+            itemImageRepository.delete(itemImage);
+        }
+    }
+
+    public void deleteUserProfileImage(Long imageId){
+        Optional<UserProfileImage> profileImage = userProfileImageRepository.findById(imageId);
+        if (profileImage.isPresent()){
+            UserProfileImage image = profileImage.get();
+
+            userProfileImageRepository.delete(image);
+        }
+    }
+
+    private void deleteFile(String path){
+        File file = new File(path);
+        if (file.exists() && file.isFile()){
+            if (file.delete()){
+                log.info("파일 삭제 완료 - " + path);
+            }
+        }
+    }
     private ItemImage toEntity(File file, Item item){
         return ItemImage.builder()
                 .fileName(file.getName())
---
+++ src/main/java/com/karrot/demo/web/api/ImageApiController.java
@@ -0,0 +1,26 @@
+package com.karrot.demo.web.api;
+
+import com.karrot.demo.service.ImageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/images")
+public class ImageApiController {
+
+    private ImageService imageService;
+    @Autowired
+    public ImageApiController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @DeleteMapping("/{imageId}")
+    public ResponseEntity deleteImage(@PathVariable Long imageId,
+                                      @RequestParam String type) {
+        imageService.deleteImage(imageId, type);
+        return ResponseEntity.ok().build();
+    }
+}
