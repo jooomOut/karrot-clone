425a56a551171c6d0230b61c21b764445cf6078d
jooomOut <rimeilo324@naver.com>
2022-10-05 14:57:42 +0900

    Chore: 패키지 import 정리

--- src/main/java/com/karrot/demo/NumbleKarrotApplication.java
+++ src/main/java/com/karrot/demo/NumbleKarrotApplication.java
@@ -1,10 +1,7 @@
 package com.karrot.demo;
 
-import com.karrot.demo.filter.ApiLoggingFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NumbleKarrotApplication {
