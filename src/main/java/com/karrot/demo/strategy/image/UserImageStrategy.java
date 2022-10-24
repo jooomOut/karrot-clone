cbd0f3ec7436ff17b8d77f0adbe6dbb457e22f16
jooomOut <rimeilo324@naver.com>
2022-10-24 22:56:12 +0900

    Add: image strategy 클래스 추가
    삭제 로직 분리

---
+++ src/main/java/com/karrot/demo/strategy/image/UserImageStrategy.java
@@ -0,0 +1,28 @@
+package com.karrot.demo.strategy.image;
+
+import com.karrot.demo.domain.image.ImageType;
+import com.karrot.demo.domain.image.UserProfileImage;
+import com.karrot.demo.domain.image.UserProfileImageRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class UserImageStrategy implements ImageStrategy {
+    private final UserProfileImageRepository userProfileImageRepository;
+
+    @Override
+    public void deleteImage(Long id) {
+        Optional<UserProfileImage> profileImage = userProfileImageRepository.findById(id);
+        if (profileImage.isPresent()){
+            UserProfileImage image = profileImage.get();
+
+            userProfileImageRepository.delete(image);
+        }
+    }
+
+    @Override
+    public ImageType getImageType() {
+        return ImageType.USER_PROFILE;
+    }
+}
